09910c5b10e62c9f5b4b25f18927986208e1500b
Vitto50 <vittorio.toffolutti@gmail.com>
2023-11-09 19:29:33 +0100

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
-import App from '../../../../../App.js';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
